95ade64dfd21a9623fe4fff1388888a7cc50fe61
aadhya-arora <aadhya270805@gmail.com>
2024-09-24 14:43:58 +0530

    js-included

--- sun.js
+++ sun.js
@@ -5,3 +5,31 @@ likeIcons.forEach((icon) => {
     icon.classList.toggle("filled");
   });
 });
+
+function expand() {
+  document.getElementById("more1").style.display = "flex";
+  document.getElementById("sunglasses").style.height = "1060px";
+  document.querySelector(".cta1").style.display = "none";
+  document.querySelector(".cta3").style.display = "flex";
+}
+
+function hide() {
+  document.getElementById("more1").style.display = "none";
+  document.getElementById("sunglasses").style.height = "600px";
+  document.querySelector(".cta1").style.display = "flex";
+  document.querySelector(".cta3").style.display = "none";
+}
+
+function expand1() {
+  document.getElementById("more2").style.display = "flex";
+  document.getElementById("scarf_done").style.height = "1060px";
+  document.querySelector(".cta2").style.display = "none";
+  document.querySelector(".cta4").style.display = "flex";
+}
+
+function hide1() {
+  document.getElementById("more2").style.display = "none";
+  document.getElementById("scarf_done").style.height = "600px";
+  document.querySelector(".cta2").style.display = "flex";
+  document.querySelector(".cta4").style.display = "none";
+}
