271b05b245b074572e77518581555f9db064a2bf
aadhya-arora <aadhya270805@gmail.com>
2024-09-25 14:50:14 +0530

    buttons changed

--- frag.js
+++ frag.js
@@ -26,23 +26,27 @@ window.addEventListener("scroll", scroll);
 function fun1() {
   document.getElementById("more").style.display = "flex";
   document.getElementById("perfumes").style.height = "1050px";
+  document.querySelector(".cta").style.display = "none";
+  document.querySelector(".cta2").style.display = "flex";
 }
 
-function fun2() {
+function hide() {
   document.getElementById("more").style.display = "none";
   document.getElementById("perfumes").style.height = "600px";
+  document.querySelector(".cta").style.display = "flex";
+  document.querySelector(".cta2").style.display = "none";
 }
-
-document.querySelector("#button").addEventListener("dblclick", fun2);
 
 function fun3() {
   document.getElementById("more2").style.display = "flex";
   document.getElementById("beauty").style.height = "1050px";
+  document.querySelector(".cta1").style.display = "none";
+  document.querySelector(".cta3").style.display = "flex";
 }
 
-function fun4() {
+function hide1() {
   document.getElementById("more2").style.display = "none";
   document.getElementById("beauty").style.height = "600px";
+  document.querySelector(".cta1").style.display = "flex";
+  document.querySelector(".cta3").style.display = "none";
 }
-
-document.querySelector("#button2").addEventListener("dblclick", fun4);
