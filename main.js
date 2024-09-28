37c954d4c73aeab6ea6404ffa4fe134264f2d740
aadhya-arora <aadhya270805@gmail.com>
2024-09-28 19:02:18 +0530

    modified

---
+++ main.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById("modal");
+var span = document.getElementsByClassName("close")[0];
+
+window.onload = function () {
+  if (window.innerWidth >= 1024) {
+    modal.style.display = "block";
+  } else {
+    modal.style.display = "none";
+  }
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
